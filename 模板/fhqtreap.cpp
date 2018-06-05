dd1c867920fd188fd15c22bdbed7e947ad28dc76
LeavesLin <ljzves@gmail.com>
2018-06-05 10:50:38 +0800

    将fhqtreap模块化

--- 模板/fhqtreap.cpp
+++ 模板/fhqtreap.cpp
@@ -1,94 +1,110 @@
 #include <bits/stdc++.h>
-struct TreapNode {
-  TreapNode *ch[2];
-  int val, rnd, size;
-  TreapNode(int x) {
-    val = x; rnd = rand(); size = 1;
-    ch[0] = ch[1] = NULL;
+const int kN = 1e5+10;
+struct Node {
+  Node *ch[2];
+  int val, pri, size;
+  void maintain() {
+    size = 1;
+    if (ch[0]) size += ch[0]->size;
+    if (ch[1]) size += ch[1]->size;
   }
-};
-void update(TreapNode *u) {
-  u->size = 1;
-  if (u->ch[0]) u->size += u->ch[0]->size;
-  if (u->ch[1]) u->size += u->ch[1]->size;
-}
-void split(TreapNode *u, int k, TreapNode *&x, TreapNode *&y) {
-  if (u == NULL) x = y = NULL;
-  else {
-    if (u->val <= k) {
-      x = u; split(u->ch[1], k, u->ch[1], y);
-    } else {
-      y = u; split(u->ch[0], k, x, u->ch[0]);
+}pool[kN];
+int cnt = 0;
+class FhqTreap {
+ public:
+  Node *root, *x, *y, *z;
+  void insert(int val) {
+    split(root, val, x, y);
+    root = merge(merge(x, getNewNode(val)), y);
+  }
+  void del(int val) {
+    split(root, val, x, z); split(x, val-1, x, y);
+    y = merge(y->ch[0], y->ch[1]);
+    root = merge(merge(x, y), z);
+  }
+  int rank(int v) {
+    split(root, v-1, x, y);
+    int ret = (!x)?1:x->size+1;
+    root = merge(x, y); 
+    return ret;
+  }
+  int kth(Node *u, int rk) {
+    if (u->ch[0]) {
+      if (u->ch[0]->size >= rk) return kth(u->ch[0], rk);
+      rk -= u->ch[0]->size;
     }
-    update(u);
+    if (rk == 1) return u->val;
+    return kth(u->ch[1], rk-1);
   }
-}
-TreapNode* merge(TreapNode *x, TreapNode *y) {
-  if (x == NULL) return y;
-  else if (y == NULL) return x;
-  if (x == NULL && y == NULL) return NULL;
-  if (x->rnd < y->rnd) {
-    x->ch[1] = merge(x->ch[1], y);
-    update(x);
-    return x;
-  } else {
-    y->ch[0] = merge(x, y->ch[0]);
-    update(y);
-    return y;
+  void print(Node *u, int dep = 0) {
+    if (!u) return;
+    print(u->ch[0], dep+1);
+    for (int i = 1; i <= dep; ++i) printf(" "); printf("%d\n", u->val);
+    print(u->ch[1], dep+1);
   }
-}
-TreapNode *root = NULL, *x = NULL, *y = NULL, *z = NULL;
-int findkth(TreapNode *u, int rk) {
-  if (u->ch[0]) {
-    if (u->ch[0]->size >= rk) return findkth(u->ch[0], rk);
-    rk -= u->ch[0]->size;
+  int pre(int val) {
+    split(root, val-1, x, y);
+    int ret = kth(x, x->size);
+    root = merge(x, y);
+    return ret;
   }
-  if (rk == 1) return u->val;
-  return findkth(u->ch[1], rk-1);
-}
-void dfs(TreapNode *u, int dep) {
-  if (u->ch[1] != NULL) dfs(u->ch[1], dep+1);
-  printf("size : %2d |", u->size);
-  for (int i = 1; i <= dep; ++i) printf(" ");
-  printf("%d\n", u->val);
-  if (u->ch[0] != NULL) dfs(u->ch[0], dep+1);
-}
-void print() {
-  printf("--------ST---------\n");
-  dfs(root, 0);
-  printf("--------ED---------\n");
-}
+  int nxt(int val) {
+    split(root, val, x, y);
+    int ret = kth(y, 1);
+    root = merge(x, y);
+    return ret;
+  }
+ private:
+  Node* getNewNode(int x) {
+    pool[++cnt].val = x;
+    pool[cnt].pri = rand();
+    pool[cnt].size = 1;
+    return &pool[cnt];
+  }
+  void split(Node *now, int k, Node* &x, Node* &y) {
+    if (!now) {x = y = NULL; return;}
+    if (now->val <= k) 
+      x = now, split(now->ch[1], k, now->ch[1], y);
+    else
+      y = now, split(now->ch[0], k, x, now->ch[0]);
+    now->maintain();
+  }
+  Node* merge(Node *x, Node *y) {
+    if (!x || !y) return (!x)?y:x;
+    if (x->pri < y->pri) {
+      x->ch[1] = merge(x->ch[1], y);
+      x->maintain(); return x;
+    } else {
+      y->ch[0] = merge(x, y->ch[0]);
+      y->maintain(); return y;
+    }
+  }
+}tr;
 int main() {
-  // freopen("input1.in", "r", stdin);
   srand(time(0));
   int n; scanf("%d", &n);
-  while (n--) {
-    int op, num; scanf("%d%d", &op, &num);
-    if (op == 1) {
-      split(root, num, x, y);
-      root = merge(merge(x, new TreapNode(num)), y);
-    } else if (op == 2) {
-      split(root, num, x, z);
-      split(x, num-1, x, y);
-      y = merge(y->ch[0], y->ch[1]);
-      root = merge(merge(x, y), z);
-    } else if (op == 3) {
-      split(root, num-1, x, y);
-      if (x == NULL) printf("1\n");
-      else printf("%d\n", x->size+1);
-      root = merge(x, y);
-    } else if (op == 4) {
-      printf("%d\n", findkth(root, num));
-    } else if (op == 5) {
-      split(root, num-1, x, y);
-      printf("%d\n", findkth(x, x->size));
-      root = merge(x, y);
-    } else {
-      split(root, num, x, y);
-      printf("%d\n", findkth(y, 1));
-      root = merge(x, y);
+  for (int i = 1; i <= n; ++i) {
+    int op, val; scanf("%d%d", &op, &val);
+    switch (op) {
+      case 1 :
+        tr.insert(val);
+      break;
+      case 2 :
+        tr.del(val);
+      break;
+      case 3:
+        printf("%d\n", tr.rank(val));
+      break;
+      case 4:
+        printf("%d\n", tr.kth(tr.root, val));
+      break;
+      case 5:
+        printf("%d\n", tr.pre(val));
+      break;
+      case 6:
+        printf("%d\n", tr.nxt(val));
+      break;
     }
-    // print();
   }
   return 0;
 }
